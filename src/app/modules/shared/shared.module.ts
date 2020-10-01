eef7ba987cc0f36067e94c75457efabeafbc80c3
Rui Wang <rui.wang.624@gmail.com>
2020-09-30 20:19:58 -0400

    added mat native date module and mat date picker module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -18,6 +18,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatListModule} from '@angular/material/list';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -44,6 +46,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatListModule,
     MatExpansionModule,
     MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
